6ae1fb32e49d8ef7e63d228912e2b6f303f2ac7f
youdame <112458620+youdame@users.noreply.github.com>
2025-02-26 14:40:23 +0900

    완성

--- js/index.js
+++ js/index.js
@@ -1,19 +1,43 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+document.addEventListener("DOMContentLoaded", function () {
+  const commentForm = document.getElementById("comment-form");
+  const commentInput = document.getElementById("comment-input");
+  const commentList = document.getElementById("comment-list");
 
-console.log("알맞은 스크립트를 작성하세요");
+  // 댓글 추가 기능
+  document
+    .getElementById("submit-button")
+    .addEventListener("click", function () {
+      const commentText = commentInput.value.trim(); // 공백 제거
+
+      if (commentText === "") {
+        alert("댓글을 입력해주세요!");
+        return;
+      }
+
+      // 새로운 댓글 요소 생성
+      const newComment = document.createElement("li");
+      newComment.innerHTML = `
+          <div class="comment-item">
+              <div class="comment-author">
+                  <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+                  <span>방문자</span>
+              </div>
+              <div class="comment-content">${commentText}</div>
+          </div>
+      `;
+
+      // 댓글 리스트에 추가
+      commentList.appendChild(newComment);
+
+      // 입력창 비우기
+      commentInput.value = "";
+    });
+
+  // Enter 키로 댓글 등록
+  commentInput.addEventListener("keypress", function (event) {
+    if (event.key === "Enter" && !event.shiftKey) {
+      event.preventDefault(); // 기본 엔터 동작 방지
+      document.getElementById("submit-comment").click();
+    }
+  });
+});
